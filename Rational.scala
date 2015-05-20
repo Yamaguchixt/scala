d43da2913a3b24a621e14818886d54931dba2ab7
Yuta Yamaguchi <masuyama40332xt@gmail.com>
2015-05-20 11:05:33 +0900

    first commit by command_line

---
+++ Rational.scala
@@ -0,0 +1,26 @@
+package scalatest
+
+/**
+ * @author study
+ */
+class Rational (n: Int, d: Int) {
+  require (d != 0)
+  private val g = gcd(n.abs,d.abs)
+  val numer = n / g
+  val denom = d / g
+  def this(n: Int) = this(n,1)
+  def + (that: Rational): Rational =
+    new Rational(
+        numer * that.denom + that.numer * denom,
+        denom * that.denom
+    )
+  def + (i: Int):Rational = {
+    new Rational(numer + i * denom,denom)
+  }
+  
+  override def toString = numer + "/" + denom
+    
+  def gcd(a: Int,b: Int):Int = {
+   if(b == 0) a else gcd(b, a % b)
+  }
+}
