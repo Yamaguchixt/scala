eae50099a8698c78b055e00dec8be11f5600c266
Yamaguchixt <masuyama40332xt@gmail.com>
2015-06-13 17:31:02 +0900

    update

--- implicitSample.scala
+++ implicitSample.scala
@@ -22,3 +22,17 @@ def maxListImplicit[T](elements: List[T])(implicit orderer: T => Ordered[T]): T 
       if ( orderer(x) > tmpMax) x
       else tmpMax
 }
+
+//さらにこう書き直せる。
+x > が orderer(x) > に
+maxListImplicit(xs)にパラメータリスト(orderer)が追加される。
+
+def maxListImplicit[T](elements: List[T])(implicit orderer: T => Ordered[T]): T =
+  elements match {
+    case List() => throw new IllegalArgumentException("empty list!")
+    case List(x) => x
+    case x :: xs =>
+      val tmpMax = maxListImplicit(xs)  //2回使うからbindしとく
+      if ( x > tmpMax) x
+      else tmpMax
+}
